43449fb74717383c317e4de9de64fbc4e86943d4
Fl9b0y <jmuda918@gmail.com>
2025-07-19 00:00:07 +0200

    added new shapes

---
+++ player.gd
@@ -0,0 +1,13 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.z+=0.02
+	if Input.is_action_pressed("ui_accept"):
+		apply_impulse(Vector3.UP*100)
