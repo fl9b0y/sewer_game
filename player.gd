ba2fec0e9d760a03f39896aafd2354ca3e67dd2a
Fl9b0y <jmuda918@gmail.com>
2025-07-19 17:30:01 +0200

    Jump mechanic

--- player.gd
+++ player.gd
@@ -1,4 +1,5 @@
 extends RigidBody3D
+var multiplier : int = 1
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,6 +9,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.z+=0.02
-	if Input.is_action_pressed("ui_accept"):
-		apply_impulse(Vector3.UP*100)
+	
+	if Input.is_action_just_pressed("ui_up"):
+		apply_impulse(Vector3.UP*200*delta)
+	if Input.is_action_pressed("ui_right"):
+		apply_force(Vector3(0,0,15))
+	if Input.is_action_pressed("ui_left"):
+		apply_force(Vector3(0,0,-15))
+	if Input.is_action_just_pressed("ui_down"):
+		apply_impulse(Vector3.UP*-200*delta)
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		self.mass = 20
+		gravity_scale *=-1
+		apply_impulse(Vector3(0,multiplier*1,0))
+		multiplier*=-1
+		self.mass = 1 
+		print(gravity_scale)
+		
